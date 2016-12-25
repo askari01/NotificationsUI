c66b1954605553cb7fb6fc1b7a15496d6fa59831
Farrukh Askari <farrukh.askari01@gmail.com>
2016-12-25 18:19:49 +0500

    notification setup

--- NotificationsUI/ViewController.swift
+++ NotificationsUI/ViewController.swift
@@ -7,19 +7,13 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func datePickerDidSelectNewDate(_ sender: UIDatePicker) {
+        let selectedDate = sender.date
+        let delegate = UIApplication.shared.delegate as? AppDelegate
+        delegate?.scheduleNotification(at: selectedDate)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
